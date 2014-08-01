1dc667b11ae9954e3cbe933ca063e2d80cf352d9
scyk123 <dntspmmepls@gmail.com>
2014-07-31 17:23:38 -0700

    update readme; simplify Java FizzBuzz; No executables; FizzBuzz newlines

--- Java/FizzBuzz/src/Main.java
+++ Java/FizzBuzz/src/Main.java
@@ -1,7 +1,12 @@
 public class Main {
 
   public static void main(String[] args) {
-    FizzBuzz fizzbuzz = new FizzBuzz(100);
+    for (int i = 1; i <= 100; i++) {
+      if (i % 15 == 0) { System.out.println("FizzBuzz"); }
+      else if (i % 5 == 0) { System.out.println("Buzz"); }
+      else if (i % 3 == 0) { System.out.println("Fizz"); }
+      else { System.out.println(i); }
+    }
   }
   
 }
--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 Example Code
+
+These are projects too simple for their own repositories, typically learning exercises.
